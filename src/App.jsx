daaf422675b93f697d58f98ad0ecd0a57a4a0e7a
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-03-21 11:40:17 +0100

    called on own API (yesterday) and added functional input form (today)

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [name, setName] = useState("");
+  const [kind, setKind] = useState("");
+
+  const apiCall = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/wonderland");
+      setCharacters(response.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const postCharacter = async (e) => {
+    e.preventDefault();
+    try {
+      const newCharacter = await axios.get(
+        "http://localhost:3000/api/wonderland",
+        {
+          name: name,
+          kind: kind,
+        }
+      );
+    } catch (err) {
+      setError(err);
+    } finally {
+      apiCall();
+      setName("");
+      setKind("");
+    }
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  console.log(name, kind);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Uh oh - Something went wrong</p>;
+
+  return (
+    <>
+      <h2>Characters in 'Alice in Wonderland'</h2>
+
+      {characters.map((character) => {
+        return (
+          <>
+            <div key={character.id}>
+              <p>This is {character.name},</p>
+              <p>They are a {character.kind}.</p>
+            </div>
+          </>
+        );
+      })}
+      <div>
+        <h4>Add the other characters:</h4>
+        <form onSubmit={postCharacter}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Kind"
+            value={kind}
+            onChange={(e) => setKind(e.target.value)}
+          />
+          <button type="submit">Add Character</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
